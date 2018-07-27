f501aedb0e40e6b14338a6c99b38ecd98067044b
Charlie Jonas <devel@charliejonas.co.uk>
2018-07-27 18:28:02 +0100

    Reconfigures root URL of static page and sets explicit homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  root :to => 'high_voltage/pages#show', id: 'index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/login' => 'sessions#new', :as => :signin
   get '/logout' => 'sessions#destroy', :as => :signout
