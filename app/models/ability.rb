137940858390aff185a55b6bad6b374382e383fa
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-08 23:08:41 +0100

    Refactor for better clarity

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # setup as a guest if need be
-    if user.admin?
-      can :manage, :all # admins can do anything
-    else
-      if !user.id.nil?
+    can :read, Booking # everyone can view bookings
+    can :read, Venue # everyone can view venues
+    if user.present?  # additional permissions for logged in users
+      if user.admin?
+        can :manage, :all # admins can do anything
+      else
         can :create, Booking # users can create new bookings
         can :manage, Booking, user_id: user.id # users have full control of their own bookings
         can :manage, User, id: user.id # users have full control of their own user account
       end
-      can :read, Booking # users & guests can view all bookings
-      can :read, Venue # users & guests can view all venues
     end
   end
 end
