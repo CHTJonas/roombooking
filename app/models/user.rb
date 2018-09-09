666005c4731917a3b76eef7eb105987d44a36eb9
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-09 21:16:22 +0100

    Add Camdram API code to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,52 @@ class User < ApplicationRecord
     self.admin = false
   end
 
+  # Returns the last CamdramToken object stored in the database that belongs to the user.
+  def latest_camdram_token
+    return CamdramToken.where(user_id: self.id).last
+  end
+
+  def authorised_camdram_shows
+    venues = ['adc-theatre', 'adc-theatre-larkum-studio', 'adc-theatre-bar', 'corpus-playroom'] # Holds Camdram venues we care about.
+    all_shows = Array.new # Holds shows we get from Camdram API.
+    shows = Array.new # Holds shows we return to the caller.
+    if self.admin
+      # Get all upcoming shows in venues we care about.
+      venues.each { |venue| all_shows += camdram.get_venue(venue).shows }
+    else
+      # Get the user's upcoming shows in venues we care about.
+      all_shows += camdram.user.get_shows.reject { |x| !venues.include? x.venue.slug }
+    end
+    all_shows.each do |show|
+      # We only care about upcoming shows not shows in the past.
+      if show.performances.last.end_date > Time.now
+        shows << [show.name, show.id]
+      end
+    end
+    return shows
+  end
+
+  def authorised_camdram_societies
+    all_societies = Array.new # Holds societies we get from Camdram API.
+    societies = Array.new# Holds societies we return to the caller.
+    if self.admin
+      # Admins can make bookings on behalf of any society
+      all_societies += camdram.get_orgs
+    else
+      # Users can make bookings on behalf of societies they administer on Camdram
+      all_societies += camdram.user.get_orgs
+    end
+    all_societies.each do |society|
+      societies << [society.name, society.id]
+    end
+    return societies
+  end
+
+  def camdram
+    @camdram ||= Camdram::Client.new do |config|
+      config.api_token = latest_camdram_token.token
+      config.user_agent = "ADC Room Booking System/#{Roombooking::VERSION}"
+    end
+  end
+
 end
