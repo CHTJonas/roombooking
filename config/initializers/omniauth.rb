f31d5348e2968e114c3d68eefaadb809c9ce1f7a
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-11 23:03:49 +0100

    Take OmniAuth details from credential store

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :camdram, ENV['CAMDRAM_APP_ID'], ENV['CAMDRAM_APP_SECRET'], scope: "user_shows user_orgs user_email"
+  id = Rails.application.credentials.dig(:camdram, :app_id)
+  secret = Rails.application.credentials.dig(:camdram, :app_secret)
+  provider :camdram, id, secret, scope: "user_shows user_orgs user_email"
 end
