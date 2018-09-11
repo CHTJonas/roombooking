df6a608acf277fecc1cfaaf210b230e3bf225d1b
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-11 16:19:00 +0100

    Filter more confidential parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,8 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:secret]
+Rails.application.config.filter_parameters += [:api_key]
+Rails.application.config.filter_parameters += [:credentials]
+Rails.application.config.filter_parameters += [:token]
+Rails.application.config.filter_parameters += [:refresh_token]
