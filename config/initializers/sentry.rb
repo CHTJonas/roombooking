3cc38030b2854ec3ffa929a9f72000bef4730cd8
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-11 17:18:05 +0100

    Include POST data and cookies in Sentry reports

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,8 @@
 Raven.configure do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.sanitize_fields += ['_roombooking_session']
+  config.processors -= [Raven::Processor::PostData] # Do this to send POST data
+  config.processors -= [Raven::Processor::Cookies] # Do this to send cookies
+  config.release = Roombooking::VERSION
 end if Rails.env.production?
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
+# Don't forget to update this in config/initializers/sentry.rb as well!
 Rails.application.config.session_store :cookie_store, key: '_roombooking_session'
