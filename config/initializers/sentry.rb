7404ac61996f9872bcab812ad39718693e576aaf
Charlie Jonas <devel@charliejonas.co.uk>
2018-09-11 21:55:12 +0100

    Submit Sentry issues with background worker

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,7 @@ Raven.configure do |config|
   config.processors -= [Raven::Processor::PostData] # Do this to send POST data
   config.processors -= [Raven::Processor::Cookies] # Do this to send cookies
   config.release = Roombooking::VERSION
+  config.async = lambda { |event|
+    SentryJob.perform_later(event.to_hash)
+  }
 end if Rails.env.production?
