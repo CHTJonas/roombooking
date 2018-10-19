cc37e851c98275e2937f72bf93d0b6c54d3b9230
Charlie Jonas <devel@charliejonas.co.uk>
2018-10-19 22:08:39 +0100

    ActionMailer config for remote SMTP servers

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec unicorn -c config/unicorn.rb
 worker: bundle exec sidekiq -t 25 -q roombooking_production_default
+mailer: bundle exec sidekiq -t 25 -q roombooking_production_mailers
---
+++ config/initializers/smtp.rb
@@ -0,0 +1,13 @@
+if Rails.application.credentials.dig(:smtp, :host)
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address                => Rails.application.credentials.dig(:smtp, :host),
+    :port                   => Rails.application.credentials.dig(:smtp, :port),
+    :user_name              => Rails.application.credentials.dig(:smtp, :username),
+    :password               => Rails.application.credentials.dig(:smtp, :password),
+    :authentication         => Rails.application.credentials.dig(:smtp, :auth),
+    :enable_starttls_auto   => Rails.application.credentials.dig(:smtp, :starttls) == 1
+  }
+else
+  ActionMailer::Base.delivery_method = :sendmail
+end
