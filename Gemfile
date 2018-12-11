ed44f3f7ea5e576d2cf202a75228481e70bbd120
Charlie Jonas <charlie@charliejonas.co.uk>
2018-12-11 22:26:59 +0000

    Improve efficiency of event-generating iteration

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'linked-list', '~> 0.0.13'
 # Use Redis adapter to run Action Cable in production
 gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,5 @@
 class VenuesController < ApplicationController
+  helper_method :events_for
 
   def index
     @venues = Venue.accessible_by(current_ability, :read)
@@ -54,6 +55,28 @@ class VenuesController < ApplicationController
     alert = { 'class' => 'success', 'message' => "Deleted #{@venue.name}!"}
     flash[:alert] = alert
     redirect_to venues_path
+  end
+
+  protected
+
+  def events_for(venue)
+    start_date = (params[:start_date] ? Date.parse(params[:start_date]) : Date.today).beginning_of_week
+    end_date = start_date + 6.days
+
+    daily_bookings = venue.booking.where("repeat_until IS NULL").where(start_time: start_date..end_date).accessible_by(current_ability, :read)
+    daily_events = Array.new(daily_bookings.length)
+    daily_bookings.to_a.each_with_index { |val, index| daily_events[index] = Event.create_from_booking(val) }
+
+    repeat_events = LinkedList::List.new
+    repeat_bookings = venue.booking.where(repeat_until: start_date..end_date).accessible_by(current_ability, :read)
+    repeat_bookings.to_a.each do |booking|
+      (booking.start_time.to_date..booking.repeat_until).each do |date|
+        offset = date - booking.start_time.to_date
+        repeat_events.push Event.create_from_booking(booking, offset)
+      end
+    end
+
+    daily_events + repeat_events.to_a
   end
 
   private
