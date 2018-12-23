9325d87decbe1bbd8e6a4ad95af8a9576d24e88e
Charlie Jonas <charlie@charliejonas.co.uk>
2018-12-23 13:59:21 +0000

    Add user edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,36 @@ class UsersController < ApplicationController
     @users = User.accessible_by(current_ability, :read)
   end
 
-  # Show all information stored about a user.
+  # Edit a particular user.
+  def edit
+    @user = User.find(params[:id])
+    authorize! :edit, @user
+  end
+
+  # Update a user's fields in the database.
+  def update
+    @user = User.find(params[:id])
+    authorize! :edit, @user
+    if @user.update(user_params)
+      alert = { 'class' => 'success', 'message' => "Updated user account #{@user.id}!"}
+      flash[:alert] = alert
+      redirect_to @user
+    else
+      alert = { 'class' => 'danger', 'message' => @user.errors.full_messages.first }
+      flash.now[:alert] = alert
+      render :edit
+    end
+  end
+
+  # Show all information stored about a single user.
   def show
     @user = User.find(params[:id])
     authorize! :read, @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :blocked)
   end
 end
