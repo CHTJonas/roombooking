defb50617467ce2a10af144617d349b6ba3f1600
Charlie Jonas <charlie@charliejonas.co.uk>
2018-12-27 12:22:09 +0000

    Add useful content to homepage

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,31 @@ class Venue < ApplicationRecord
   has_many :booking
 
   validates :name, presence: true
+
+  def currently_booked?
+    !self.current_booking.nil?
+  end
+
+  def current_booking
+    now = DateTime.now
+    query = self.booking.find_by(approved: true, repeat_mode: :none, start_time: Time.at(0)..now, end_time: now..DateTime::Infinity.new)
+    return query unless query.nil?
+    self.booking.where(approved: true, repeat_mode: :daily, start_time: Time.at(0)..now, repeat_until: now..DateTime::Infinity.new).each do |booking|
+      if bkg.start_time.seconds_since_midnight < now.seconds_since_midnight
+        if bkg.end_time.seconds_since_midnight > now.seconds_since_midnight
+          return bkg
+        end
+      end
+    end
+    self.booking.where(approved: true, repeat_mode: :weekly, start_time: Time.at(0)..now, repeat_until: now..DateTime::Infinity.new).each do |booking|
+      if bkg.start_time.wday = now.wday
+        if bkg.start_time.seconds_since_midnight < now.seconds_since_midnight
+          if bkg.end_time.seconds_since_midnight > now.seconds_since_midnight
+            return bkg
+          end
+        end
+      end
+    end
+    nil
+  end
 end
