73a90fffc21c1d6696cbbb513e1c97f713546354
Charlie Jonas <charlie@charliejonas.co.uk>
2019-02-02 13:33:44 +0000

    Don't care about rails restart

--- config/puma.rb
+++ config/puma.rb
@@ -13,9 +13,6 @@ if environment == 'production' || ENV['BIND']
   bind 'unix:///var/run/roombooking/app_serv.sock?umask=0077'
 end
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
-
 # We don't need to worry about ActiveRecord since we're not preloading.
 before_fork do
   # ActiveRecord::Base.connection_pool.disconnect!
