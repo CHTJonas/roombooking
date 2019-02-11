3b800aa109d3aef8d6c0601e3b61cf221710f982
Charlie Jonas <charlie@charliejonas.co.uk>
2019-02-11 21:50:51 +0000

    Fix AJAX response issue caused by escape_utils gem

--- config/initializers/escape_utils.rb
+++ config/initializers/escape_utils.rb
@@ -6,4 +6,3 @@ require 'escape_utils/url/cgi' # to patch CGI
 require 'escape_utils/url/erb' # to patch ERB::Util
 require 'escape_utils/url/rack' # to patch Rack::Utils
 require 'escape_utils/url/uri' # to patch URI
-require 'escape_utils/javascript/action_view' # to patch ActionView::Helpers::JavaScriptHelper
