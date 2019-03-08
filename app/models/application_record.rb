8bbd2fb54f038f130f794362a3244c7b7eda8706
Charlie Jonas <charlie@charliejonas.co.uk>
2019-03-08 00:18:22 +0000

    Add support for Postgres' advisory locks

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
   has_paper_trail
 
-  self.abstract_class = true
+  private
+
+  def exclusive
+    if obtain_pg_lock
+      begin
+        yield
+      ensure
+        release_pg_lock
+      end
+    end
+  end
+
+  def obtain_pg_lock
+    connection.execute("SELECT pg_advisory_lock(#{self.id});")
+  end
+
+  def release_pg_lock
+    connection.execute("SELECT pg_advisory_unlock(#{self.id});")
+  end
+
+  def connection
+    ActiveRecord::Base.connection
+  end
 end
