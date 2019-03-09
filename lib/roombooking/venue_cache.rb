7ac2c20cf04ae59f71c5e6f34fb9d6bdb2686ff8
Charlie Jonas <charlie@charliejonas.co.uk>
2019-03-09 19:11:24 +0000

    Use the Rails cache as distributed store for Camdram venues

--- lib/roombooking/venue_cache.rb
+++ lib/roombooking/venue_cache.rb
@@ -4,29 +4,37 @@ module Roombooking
   module VenueCache
     class << self
       def each(&block)
-        cache.each(&block)
+        venues.each(&block)
+        nil
       end
 
       def contains?(o)
-        cache.include?(o)
+        venues.include?(o)
       end
 
       def regenerate
-        @cache = nil
-        cache
+        Rails.cache.write(cache_key, set_of_venues.to_a)
         nil
       end
 
       private
 
-      def cache
-        @cache ||= (
-          set = Concurrent::Set.new
-          Room.all.each do |room|
-            set.merge(room.camdram_venues) if room.camdram_venues
-          end
-          set
-        )
+      def venues
+        Rails.cache.fetch(cache_key) do
+          set_of_venues.to_a
+        end
+      end
+
+      def set_of_venues
+        set = Set.new
+        Room.all.each do |room|
+          set.merge(room.camdram_venues) if room.camdram_venues
+        end
+        set
+      end
+
+      def cache_key
+        'rbCamdramVenues'
       end
     end
   end
