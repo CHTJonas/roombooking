a2ccd39cce512dec374b5d55874b405fc5379b4e
Charlie Jonas <charlie@charliejonas.co.uk>
2019-03-14 23:49:17 +0000

    Ensure abuse logger doesn't log to Rails' default log file

--- config/initializers/abuse_logging.rb
+++ config/initializers/abuse_logging.rb
@@ -2,6 +2,6 @@
 # See: https://github.com/rudionrails/yell/issues/50
 
 abuse_log_file = Rails.root.join('log'.freeze, "roombooking_#{Rails.env}_abuse.log")
-abuse_logger = Yell.new(format: '%d [ABUSE] %p : %m')
-abuse_logger.adapter(:datefile, abuse_log_file, keep: 31)
-Yell['abuse'.freeze] = abuse_logger
+Yell['abuse'.freeze] = Yell.new(format: '%d [ABUSE] %p : %m') do |l|
+  l.adapter(:datefile, abuse_log_file, keep: 31)
+end
