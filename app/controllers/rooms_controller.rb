57d6b792271d3337862695541b2f96493983ba5c
Charlie Jonas <charlie@charliejonas.co.uk>
2019-03-20 22:30:57 +0000

    Improve DB queries for room HTML & iCal

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -53,12 +53,15 @@ class RoomsController < ApplicationController
   end
 
   def show
-    @room = Room.eager_load(approved_bookings: :user)
-      .then { |_| request.format == :ics ? _.preload(approved_bookings: :camdram_model) : _ }.find(params[:id])
+    @room = Room.then { |_| request.format == :ics ? _.eager_load(approved_bookings: [:room, :user])
+      .preload(approved_bookings: :camdram_model) : _ }
+      .find(params[:id])
     authorize! :read, @room
-    start_date = (params[:start_date] ? Date.parse(params[:start_date]) : Date.today).beginning_of_week
-    end_date = start_date + 7.days
-    @events = @room.events_in_range(start_date, end_date)
+    unless request.format == :ics
+      start_date = (params[:start_date] ? Date.parse(params[:start_date]) : Date.today).beginning_of_week
+      end_date = start_date + 7.days
+      @events = @room.events_in_range(start_date, end_date)
+    end
     respond_to do |format|
       format.html
       format.ics
