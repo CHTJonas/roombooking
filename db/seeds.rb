43b31c4fce0cdbbe99540281df2cd7d10dffd33c
Charlie Jonas <charlie@charliejonas.co.uk>
2019-03-23 22:03:09 +0000

    Split Playroom dressing rooms into separate venues

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ Room.create(name: 'Dressing Room 1', camdram_venues: ['adc-theatre', 'adc-theatr
 Room.create(name: 'Dressing Room 2', camdram_venues: ['adc-theatre', 'adc-theatre-larkum-studio', 'adc-theatre-bar'])
 Room.create(name: 'Bar', camdram_venues: ['adc-theatre', 'adc-theatre-larkum-studio', 'adc-theatre-bar'])
 Room.create(name: 'Playroom Auditorium', camdram_venues: ['corpus-playroom'])
-Room.create(name: 'Playroom Dressing Rooms', camdram_venues: ['corpus-playroom'])
+Room.create(name: 'Playroom Dressing Room 1', camdram_venues: ['corpus-playroom'])
+Room.create(name: 'Playroom Dressing Room 2', camdram_venues: ['corpus-playroom'])
 User.create(name: "Charlie Jonas", email: "charlie@charliejonas.co.uk", admin: true, sysadmin: true, blocked: false)
 ProviderAccount.create(provider: "camdram", uid: "3807", user_id: 1)
