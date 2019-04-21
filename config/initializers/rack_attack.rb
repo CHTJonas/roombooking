922ce0a0e7730f4a854c6753d86c693449bfae55
Charlie Jonas <charlie@charliejonas.co.uk>
2019-04-21 16:59:06 +0100

    Revisit Rack::Attack config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,16 +5,18 @@ Rails.application.config.middleware.use Rack::Attack
 # Blacklist IPs using the cache
 # To add an IP: Rails.cache.write("blocklist 1.2.3.4", true, expires_in: 2.days)
 # To remove an IP: Rails.cache.delete("blocklist 1.2.3.4")
-Rack::Attack.blocklist("block IP") do |req|
-  Rails.cache.read("blocklist #{req.ip}")
+Rack::Attack.blocklist('block IP') do |request|
+  Rails.cache.read("blocklist #{request.ip}")
 end
 
-# Throttle all requests by IP (60rpm)
-Rack::Attack.throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-  req.ip
+# Limit all IP addresses to 60rpm.
+Rack::Attack.throttle('requests by ip', limit: 300, period: 5.minutes) do |request|
+  request.ip
 end
 
-# Prevent brute-force login attempts
-Rack::Attack.throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
-  req.path == '/login' and req.ip
+# Prevent brute-force login or 2FA attempts.
+Rack::Attack.throttle('logins by ip', limit: 5, period: 20.seconds) do |request|
+  if request.path == '/login' || request.path == '/auth/2fa'
+    request.ip
+  end
 end
