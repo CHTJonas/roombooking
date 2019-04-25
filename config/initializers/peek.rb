ef1ca247f6f63ae21dc0ce48120de4fd376e4360
Charlie Jonas <charlie@charliejonas.co.uk>
2019-04-25 22:50:06 +0100

    Fix Peek configuration

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -8,4 +8,7 @@ Peek.into Peek::Views::Sidekiq
 Peek.into Peek::Views::GC
 Peek.into Peek::Views::Rblineprof
 
-Rails.application.config.peek.adapter = :redis
+Rails.application.config.peek.adapter = :redis, {
+  client: Redis.new(url: ENV['REDIS_CACHE']),
+  expires_in: 60 * 30 # 30 minutes in seconds
+}
