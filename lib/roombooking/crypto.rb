c9105aa34bd0602068abb13024efb4c38a3a915d
Charlie Jonas <charlie@charliejonas.co.uk>
2019-05-09 22:47:20 +0100

    Test derivation of crypto keys

--- lib/roombooking/crypto.rb
+++ lib/roombooking/crypto.rb
@@ -4,14 +4,13 @@ module Roombooking
   module Crypto
     class << self
 
-      # Returns a binary-formatted key of 32 bytes in length that is derived
+      # Returns a binary sequence of 32 bytes in length that is derived
       # from Rails' secret key base. This is suitable for use with the
       # aes-256-gcm encryption algorithm of the attr_encrypted gem.
       def secret_key
         key = ENV['SECRET_KEY_BASE']
         arr = [key]
-        bytes = arr.pack("H*")
-        bytes[0..31]
+        arr.pack("H64")
       end
     end
   end
---
+++ test/integration/crypto_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class CryptoTest < ActionDispatch::IntegrationTest
+  test "secure key derivation" do
+    secure_bytes = Roombooking::Crypto.secret_key
+    assert secure_bytes.length == 32
+  end
+end
