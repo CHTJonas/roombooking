dc6300c4557bdbc2b55d2596331d1a00214eb713
Charlie Jonas <charlie@charliejonas.co.uk>
2019-05-15 21:35:40 +0100

    Implement Content-Security-Policy

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -26,5 +26,41 @@ SecureHeaders::Configuration.default do |config|
   # Prevent leaking excessive information to third-parties via the refer header.
   config.referrer_policy = "strict-origin-when-cross-origin"
 
-  config.csp = SecureHeaders::OPT_OUT
+  config.csp = {
+    # Remove schemes from host sources in order to save bytes and discourage
+    # mixed content.
+    preserve_schemes: true,
+
+    # Fetch directives
+    child_src: %w('none'),
+    connect_src: %w('self'),
+    default_src: %w('none'),
+    font_src: %w('self'),
+    frame_src: %w('none'),
+    img_src: %w('self' secure.gravatar.com),
+    manifest_src: %w('none'),
+    media_src: %w('none'),
+    object_src: %w('none'),
+    prefetch_src: %w('none'),
+    script_src: %w('self' 'unsafe-inline'),
+    style_src: %w('self' 'unsafe-inline'),
+    # webrtc_src: %w('none'),
+    worker_src: %w('none'),
+
+    # Document directives
+    base_uri: %w('self'),
+    plugin_types: %w(),
+    sandbox: false,
+
+    # Navigation directives
+    form_action: %w('self'),
+    frame_ancestors: %w('none'),
+
+    # Reporting directives
+    report_uri: %w(https://sentry.io/api/1278887/security/?sentry_key=0a2fb033999d4499bec75801fe55d575),
+
+    # Other directives
+    block_all_mixed_content: true,
+    upgrade_insecure_requests: true
+  }
 end
