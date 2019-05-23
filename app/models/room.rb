CHTJonas/roombooking

commit c6afcafb4bdddee406462d81d018666faf648035

Remove everything to do with approvals

app/models/room.rb
@@ -15,7 +15,6 @@ class Room < ApplicationRecord
   has_paper_trail
 
   has_many :booking, dependent: :destroy
-  has_many :approved_bookings, -> { where(approved: true) }, class_name: 'Booking'
 
   validates :name, presence: true
 
@@ -31,16 +30,16 @@ class Room < ApplicationRecord
   end
 
   def get_booking_at(date)
-    query = self.booking.find_by(approved: true, repeat_mode: :none, start_time: Time.at(0)..date, end_time: date..DateTime::Infinity.new)
+    query = self.booking.find_by(repeat_mode: :none, start_time: Time.at(0)..date, end_time: date..DateTime::Infinity.new)
     return query unless query.nil?
-    self.booking.where(approved: true, repeat_mode: :daily, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
+    self.booking.where(repeat_mode: :daily, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
       if bkg.start_time.seconds_since_midnight < date.seconds_since_midnight
         if bkg.end_time.seconds_since_midnight > date.seconds_since_midnight
           return bkg
         end
       end
     end
-    self.booking.where(approved: true, repeat_mode: :weekly, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
+    self.booking.where(repeat_mode: :weekly, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
       if bkg.start_time.wday == date.wday
         if bkg.start_time.seconds_since_midnight < date.seconds_since_midnight
           if bkg.end_time.seconds_since_midnight > date.seconds_since_midnight
@@ -53,6 +52,6 @@ class Room < ApplicationRecord
   end
 
   def events_in_range(start_date, end_date)
-    Event.from_bookings approved_bookings.in_range(start_date, end_date)
+    Event.from_bookings(bookings.in_range(start_date, end_date))
   end
 end

app/controllers/bookings_controller.rb
@@ -34,7 +34,6 @@ class BookingsController < ApplicationController
     if @booking.save
       NotificationJob.perform_async(@booking.id, @booking.camdram_model.try(:to_global_id).try(:to_s))
       msg = "Added #{@booking.name}!"
-      msg << " You will need to wait for this booking to be approved by an admin before it is shown publicly." unless @booking.approved?
       alert = { 'class' => 'success', 'message' =>  msg}
       flash[:alert] = alert
       redirect_to @booking
@@ -78,18 +77,6 @@ class BookingsController < ApplicationController
     alert = { 'class' => 'success', 'message' => "Deleted #{@booking.name}!" }
     flash[:alert] = alert
     redirect_to bookings_path
-  end
-
-  def approve
-    @booking = Booking.find(params[:id])
-    authorize! :approve, @booking
-    @booking.approved = true
-    if @booking.save
-      MailDeliveryJob.perform_async(ApprovalsMailer, :approve, @booking.user.id, @booking.id)
-      alert = { 'class' => 'success', 'message' => "Approved #{@booking.name}!" }
-      flash[:alert] = alert
-      redirect_to @booking
-    end
   end
 
   def favourites

app/controllers/camdram_shows_controller.rb
@@ -2,7 +2,7 @@
 
 class CamdramShowsController < ApplicationController
   def show
-    @camdram_show = CamdramShow.eager_load(:approved_bookings).find(params[:id])
+    @camdram_show = CamdramShow.eager_load(:bookings).find(params[:id])
     authorize! :read, @camdram_show
     @external_show = @camdram_show.camdram_object
     @quota = @camdram_show.weekly_quota Date.today.beginning_of_week

app/controllers/camdram_societies_controller.rb
@@ -2,7 +2,7 @@
 
 class CamdramSocietiesController < ApplicationController
   def show
-    @camdram_society = CamdramSociety.eager_load(:approved_bookings).find(params[:id])
+    @camdram_society = CamdramSociety.eager_load(:bookings).find(params[:id])
     authorize! :read, @camdram_society
     @external_society = @camdram_society.camdram_object
     @quota = @camdram_society.weekly_quota Date.today.beginning_of_week

app/controllers/rooms_controller.rb
@@ -5,8 +5,8 @@ class RoomsController < ApplicationController
     if request.format == :html
       @rooms = Room.accessible_by(current_ability, :read).order(:id)
     elsif request.format == :ics
-      @rooms = Room.eager_load(approved_bookings: [:room, :user])
-        .preload(approved_bookings: :camdram_model)
+      @rooms = Room.eager_load(bookings: [:room, :user])
+        .preload(bookings: :camdram_model)
         .accessible_by(current_ability, :read).order(:id)
     end
     respond_to do |format|
@@ -60,8 +60,8 @@ class RoomsController < ApplicationController
       @end_date = @start_date + 7.days
       @events = @room.events_in_range(@start_date, @end_date)
     elsif request.format == :ics
-      @room = Room.eager_load(approved_bookings: [:room, :user])
-        .preload(approved_bookings: :camdram_model)
+      @room = Room.eager_load(bookings: [:room, :user])
+        .preload(bookings: :camdram_model)
         .find(params[:id])
     end
     authorize! :read, @room

app/jobs/approval_reminder_job.rb
@@ -1,17 +1,0 @@
-# frozen_string_literal: true
-
-class ApprovalReminderJob
-  include Sidekiq::Worker
-  include Sidekiq::Throttled::Worker
-
-  sidekiq_options queue: 'roombooking_jobs'
-  sidekiq_throttle concurrency: { limit: 1 }
-
-  def perform
-    Booking.where(approved: false).find_each(batch_size: 5) do |booking|
-      User.where(admin: true).each do |admin|
-        MailDeliveryJob.perform_async(ApprovalsMailer, :remind, admin.id, booking.id)
-      end
-    end
-  end
-end

app/jobs/notification_job.rb
@@ -11,7 +11,6 @@ class NotificationJob
   def perform(booking_id, camdram_model_global_id)
     @booking = Booking.find(booking_id)
     @camdram_model = GlobalID::Locator.locate camdram_model_global_id if camdram_model_global_id
-    notify_admins unless @booking.approved
     if @camdram_model.present?
       notify_slack_webhook if @camdram_model.slack_webhook.present?
     end
@@ -25,12 +24,6 @@ class NotificationJob
       msg += " Description:\n#{@booking.notes}" if @booking.notes.present?
       msg
     )
-  end
-
-  def notify_admins
-    User.where(admin: true).each do |admin|
-      MailDeliveryJob.perform_async(ApprovalsMailer, :notify, admin.id, @booking.id)
-    end
   end
 
   def notify_slack_webhook

app/mailers/approvals_mailer.rb
@@ -1,25 +1,0 @@
-# frozen_string_literal: true
-
-class ApprovalsMailer < ApplicationMailer
-
-  # Notify an admin that a new booking has been created that needs approval.
-  def notify(admin_id, booking_id)
-    @admin = User.find(admin_id)
-    @booking = Booking.find(booking_id)
-    mail(to: @admin.email, subject: 'New room booking needs approval')
-  end
-
-  # Remind an admin that a booking exists which still needs approval.
-  def remind(admin_id, booking_id)
-    @admin = User.find(admin_id)
-    @booking = Booking.find(booking_id)
-    mail(to: @admin.email, subject: 'Room booking approval reminder')
-  end
-
-  # Tell a user that their booking has been approved by an admin.
-  def approve(user_id, booking_id)
-    @user = User.find(user_id)
-    @booking = Booking.find(booking_id)
-    mail(to: @user.email, subject: 'Your room booking has been approved')
-  end
-end

commit 4fb28c3772a3199f46b3b9396c35af6c4e832187

Fix annoying typo
Regression introduced in c6afcaf

app/models/room.rb
@@ -52,6 +52,6 @@ class Room < ApplicationRecord
   end
 
   def events_in_range(start_date, end_date)
-    Event.from_bookings(bookings.in_range(start_date, end_date))
+    Event.from_bookings(booking.in_range(start_date, end_date))
   end
 end