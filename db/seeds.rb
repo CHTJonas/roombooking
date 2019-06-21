dd5d8c30118f5e0c715b84877b39b910877bb46c
Charlie Jonas <charlie@charliejonas.co.uk>
2019-06-21 18:48:23 +0100

    User won't necessarily be created with correct id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 PaperTrail.enabled = false
 
-adc = CamdramVenue.create(camdram_id: 29) # ADC Theatre
-bar = CamdramVenue.create(camdram_id: 68) # ADC Theatre (Bar)
-larkum = CamdramVenue.create(camdram_id: 69) # ADC Theatre (Larkum Studio)
-playroom = CamdramVenue.create(camdram_id: 30) # Corpus Playroom
+adc = CamdramVenue.create(camdram_id: 29)
+bar = CamdramVenue.create(camdram_id: 68)
+larkum = CamdramVenue.create(camdram_id: 69)
+playroom = CamdramVenue.create(camdram_id: 30)
 adc_venues = [adc, bar, larkum]
 playroom_venues = [playroom]
 
@@ -16,5 +16,5 @@ Room.create(name: 'Playroom Auditorium', camdram_venues: playroom_venues)
 Room.create(name: 'Playroom Dressing Room 1', camdram_venues: playroom_venues)
 Room.create(name: 'Playroom Dressing Room 2', camdram_venues: playroom_venues)
 
-User.create(name: "Charlie Jonas", email: "charlie@charliejonas.co.uk", admin: true, sysadmin: true, blocked: false)
-ProviderAccount.create(provider: "camdram", uid: "3807", user_id: 1)
+cj = User.create(name: "Charlie Jonas", email: "charlie@charliejonas.co.uk", admin: true, sysadmin: true, blocked: false)
+ProviderAccount.create(provider: "camdram", uid: "3807", user: cj)
