9f9a5a29b173085ab6c75997bf47d82886b660d9
Charlie Jonas <charlie@charliejonas.co.uk>
2019-07-01 23:13:22 +0100

    Add a single simple rooms controller test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -129,7 +129,7 @@ class ApplicationController < ActionController::Base
 
   # Make sure the user is using a modern browser.
   def check_browser_version
-    if request.format == :html && (browser.ie? || !browser.modern?)
+    if Rails.env.production? && request.format == :html && (browser.ie? || !browser.modern?)
       alert = { 'class' => 'danger', 'message' => "You seem to be using a very outdated web browser! Unfortunately you'll need to update your system in order to use Room Booking." }
       flash.now[:alert] = alert
       render 'layouts/blank', locals: {reason: "outdated browser"}, status: :ok
--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should show rooms index" do
+    get rooms_url
+    assert_response :success
+    assert_select "h1", "Rooms"
+    assert_select "body > main > ul > li:nth-child(1) > a", text: "Corpus Playroom"
+    assert_select "body > main > ul > li:nth-child(2) > a", text: "ADC Theatre"
+    assert_select "body > main > div.float-right.mt-1 > a", text: "Subscribe to iCal"
+  end
 end
