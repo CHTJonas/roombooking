f83e74dadc5dbcb59be6ccd4352545563e90a8cc
Charlie Jonas <charlie@charliejonas.co.uk>
2019-07-12 21:07:15 +0100

    Move verification token check to mailer

--- app/jobs/email_verification_reminder_job.rb
+++ app/jobs/email_verification_reminder_job.rb
@@ -9,15 +9,10 @@ class EmailVerificationReminderJob
 
   def perform
     User.where(validated_at: nil).each do |user|
-      if user.validation_token.present?
-        mailer = 'EmailVerificationMailer'
-        method = 'remind'
-        user_id = user.id
-        MailDeliveryJob.perform_async(mailer, method, user_id)
-      else
-        e = Roombooking::InvalidStateError.new("Detected user (id: #{user.id}) with unverified email but no verification token")
-        Raven.capture_exception(e)
-      end
+      mailer = 'EmailVerificationMailer'
+      method = 'remind'
+      user_id = user.id
+      MailDeliveryJob.perform_async(mailer, method, user_id)
     end
   end
 end
--- app/mailers/email_verification_mailer.rb
+++ app/mailers/email_verification_mailer.rb
@@ -6,13 +6,21 @@ class EmailVerificationMailer < ApplicationMailer
   # their newly-created account.
   def notify(user_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: 'Verify your email address')
+    if @user.validation_token.present?
+      mail(to: @user.email, subject: 'Verify your email address')
+    else
+      raise Roombooking::InvalidStateError, "User with id #{@user.id} has no verification token"
+    end
   end
 
   # Sends an email to the user reminding them that they have yet to complete the
   # signup process and validate their email address.
   def remind(user_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: 'You have yet to verify your email address')
+    if @user.validation_token.present?
+      mail(to: @user.email, subject: 'You have yet to verify your email address')
+    else
+      raise Roombooking::InvalidStateError, "User with id #{@user.id} has no verification token"
+    end
   end
 end
