4abe38d07662defbcaa6a0b5d6e877bb883e40ac
Charlie Jonas <charlie@charliejonas.co.uk>
2019-07-14 01:01:33 +0100

    Explicitly allow Recaptcha in CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,7 +3,8 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src     :none
   policy.img_src         :self, "https://secure.gravatar.com"
-  policy.script_src      :self
+  policy.script_src      :self, "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/recaptcha/api.js"
+  policy.frame_src       "https://www.google.com/recaptcha/"
   policy.style_src       :self, :unsafe_inline
   policy.font_src        :self
   policy.connect_src     :self, "https://sentry.io"
