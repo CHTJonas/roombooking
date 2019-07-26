d5f02c785aeb5a114b16836975659b9060ec6672
Charlie Jonas <charlie@charliejonas.co.uk>
2019-07-26 15:34:40 +0200

    Fix potential Sidekiw threading issue in development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
-sidekiq_url = ENV['REDIS_STORE']
+sidekiq_url = ENV['REDIS_STORE'].freeze
 
 Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_url }
-
-  schedule_file = Rails.root.join('config/schedule.yml')
-  if File.exist?(schedule_file) && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
-
-  Sidekiq::QueueMetrics.init(config)
+  schedule_file = Rails.root.join('config', 'schedule.yml')
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
 Sidekiq.configure_client do |config|
