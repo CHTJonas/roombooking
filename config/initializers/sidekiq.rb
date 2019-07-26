ebe2c25363603aff702f6ae209bb560f1512faa2
Charlie Jonas <charlie@charliejonas.co.uk>
2019-07-26 14:10:05 +0200

    Remove custom Sidekiq error handler
    Error reporting is already provided automatically and with greater
    detail by the Raven::SidekiqErrorHandler class.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,6 @@ sidekiq_url = ENV['REDIS_STORE']
 
 Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_url }
-  config.error_handlers << Proc.new  do |exception, context_hash|
-    Raven.capture_exception(exception, level: 'warning')
-  end
 
   schedule_file = Rails.root.join('config/schedule.yml')
   if File.exist?(schedule_file) && Sidekiq.server?
