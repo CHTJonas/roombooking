4008bebe1666c07b772e4440ef3874233449ffa5
Charlie Jonas <charlie@charliejonas.co.uk>
2019-08-18 17:53:21 +0100

    Disable Bullet for third-party applications

--- app/controllers/legacy_content_controller.rb
+++ app/controllers/legacy_content_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class LegacyContentController < ApplicationController
+class LegacyContentController < ThirdPartyContentController
   # Disable entirely until https://github.com/rails/rails/issues/35137 is resolved.
   content_security_policy false
   # content_security_policy do |policy|
---
+++ app/controllers/third_party_content_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ThirdPartyContentController < ApplicationController
+  around_action :skip_bullet, if: -> { defined?(Bullet) }
+
+  def skip_bullet
+    previous_value = Bullet.enable?
+    Bullet.enable = false
+    yield
+  ensure
+    Bullet.enable = previous_value
+  end
+end
