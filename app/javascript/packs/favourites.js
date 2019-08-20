564de6e2a7640b0477c47152a76b92811bf976ed
Charlie Jonas <charlie@charliejonas.co.uk>
2019-08-20 18:07:46 +0100

    Tidy JavaScript files

--- app/javascript/packs/favourites.js
+++ app/javascript/packs/favourites.js
@@ -1,117 +1,107 @@
 import $ from "jquery";
 
-// Favourite bookings are stored in an indexed database locally in the user's
-// browser. This provides a level of isolation for privacy reasons.
-var addBookingToFavourites, appendFavourites, clearFavourites, setupIDB, showErrorFavourites, showFavouriteBookings;
+// Favourite bookings are stored in an indexed database locally in the user's browser.
+// This provides a level of isolation for privacy reasons.
 
-clearFavourites = () => {
-  $('#favourite-bookings-spinner').replaceWith('<p class="mt-4 my-3">None!</p>');
+const clearFavourites = () => {
+    $('#favourite-bookings-spinner').replaceWith('<p class="mt-4 my-3">None!</p>');
 };
 
-showErrorFavourites = () => {
-  $('#favourite-bookings-spinner').replaceWith('<p class="mt-4 my-3">An error occurred retrieving favourite bookings!</p>');
+const showErrorFavourites = () => {
+    $('#favourite-bookings-spinner').replaceWith('<p class="mt-4 my-3">An error occurred retrieving favourite bookings!</p>');
 };
 
-appendFavourites = (data) => {
-  if (data) {
-    $('#favourite-bookings-container').replaceWith(data);
-  } else {
-    clearFavourites();
-  }
+const appendFavourites = (data) => {
+    if (data) {
+        $('#favourite-bookings-container').replaceWith(data);
+    } else {
+        clearFavourites();
+    }
 };
 
-setupIDB = () => {
-  var DBOpenRequest;
-  DBOpenRequest = indexedDB.open("roombooking", 1);
-  DBOpenRequest.onupgradeneeded = (event) => {
-    var db, objectStore;
-    db = event.target.result;
-    db.onerror = (e) => {
-      console.error(e);
-      throw "An error occurred";
-    };
-    objectStore = db.createObjectStore("favouriteShows", {
-      keyPath: "bookingId"
-    });
-    objectStore.createIndex("numberOfVisits", "numberOfVisits", {
-      unique: false
-    });
-    objectStore.createIndex("lastVisitDate", "lastVisitDate", {
-      unique: false
-    });
-  };
-  return DBOpenRequest;
-};
-
-addBookingToFavourites = (id) => {
-  var DBOpenRequest;
-  DBOpenRequest = setupIDB();
-  return DBOpenRequest.onsuccess = (event) => {
-    var db, now, obj, objectStore, objectStoreRequest, transaction;
-    db = DBOpenRequest.result;
-    transaction = db.transaction("favouriteShows", "readwrite");
-    objectStore = transaction.objectStore("favouriteShows");
-    now = new Date();
-    obj = {
-      bookingId: id,
-      numberOfVisits: 1,
-      lastVisitDate: now.toUTCString()
-    };
-    objectStoreRequest = objectStore.get(id);
-    return objectStoreRequest.onsuccess = (event) => {
-      var record;
-      record = objectStoreRequest.result;
-      if (record) {
-        obj.numberOfVisits += record.numberOfVisits;
-      }
-      return objectStore.put(obj);
-    };
-  };
-};
-
-showFavouriteBookings = () => {
-  var DBOpenRequest, objs;
-  objs = [];
-  DBOpenRequest = setupIDB();
-  DBOpenRequest.onerror = (event) => {
-    showErrorFavourites();
-  };
-  return DBOpenRequest.onsuccess = (event) => {
-    var db, objectStore, request, transaction;
-    db = DBOpenRequest.result;
-    transaction = db.transaction("favouriteShows");
-    objectStore = transaction.objectStore("favouriteShows");
-    request = objectStore.openCursor();
-    return request.onsuccess = (event) => {
-      var cursor, obj, params;
-      cursor = event.target.result;
-      if (cursor) {
-        obj = cursor.value;
-        objs.push(obj);
-        return cursor.continue();
-      } else {
-        objs.sort(function(a, b) {
-          return b.numberOfVisits - a.numberOfVisits;
-        });
-        params = {
-          ids: objs.slice(0, 9).map(function(obj) {
-            return obj.bookingId;
-          })
+const setupIDB = () => {
+    const DBOpenRequest = indexedDB.open("roombooking", 1);
+    DBOpenRequest.onupgradeneeded = (dbOpenEvent) => {
+        const db = dbOpenEvent.target.result;
+        db.onerror = (e) => {
+            console.error(e);
+            throw "An error occurred";
         };
-        return $.post("bookings/favourites", params, appendFavourites, "html");
-      }
+        const objectStore = db.createObjectStore("favouriteShows", {
+            keyPath: "bookingId"
+        });
+        objectStore.createIndex("numberOfVisits", "numberOfVisits", {
+            unique: false
+        });
+        objectStore.createIndex("lastVisitDate", "lastVisitDate", {
+            unique: false
+        });
     };
-  };
+    return DBOpenRequest;
+};
+
+const addBookingToFavourites = (id) => {
+    const DBOpenRequest = setupIDB();
+    DBOpenRequest.onsuccess = (dbOpenEvent) => {
+        const db = DBOpenRequest.result;
+        const transaction = db.transaction("favouriteShows", "readwrite");
+        const objectStore = transaction.objectStore("favouriteShows");
+        const now = new Date();
+        const obj = {
+            bookingId: id,
+            numberOfVisits: 1,
+            lastVisitDate: now.toUTCString()
+        };
+        const objectStoreRequest = objectStore.get(id);
+        objectStoreRequest.onsuccess = (objectStoreEvent) => {
+            if (objectStoreRequest.result) {
+                obj.numberOfVisits += record.numberOfVisits;
+            }
+            objectStore.put(obj);
+        };
+    };
+};
+
+const showFavouriteBookings = () => {
+    const objects = [];
+    const DBOpenRequest = setupIDB();
+    DBOpenRequest.onerror = (dbOpenEvent) => {
+        showErrorFavourites();
+    };
+    DBOpenRequest.onsuccess = (dbOpenEvent) => {
+        const db = DBOpenRequest.result;
+        const transaction = db.transaction("favouriteShows");
+        const objectStore = transaction.objectStore("favouriteShows");
+        const request = objectStore.openCursor();
+        request.onsuccess = (objectStoreEvent) => {
+            var obj, params;
+            const cursor = objectStoreEvent.target.result;
+            if (cursor) {
+                objects.push(cursor.value);
+                cursor.continue();
+            } else {
+                objects.sort((a, b) => {
+                    return b.numberOfVisits - a.numberOfVisits;
+                });
+                params = {
+                    ids: objects.slice(0, 9).map((obj) => {
+                        return obj.bookingId;
+                    })
+                };
+                $.post("bookings/favourites", params, appendFavourites, "html")
+                        .fail(showErrorFavourites);
+            }
+        };
+    };
 };
 
 $(() => {
-  var bookingId, match, regex;
-  regex = /^\/bookings\/([0-9]+)$/i;
-  match = window.location.pathname.match(regex);
-  if (match && match.length === 2) {
-    bookingId = Number(match[1]);
-    addBookingToFavourites(bookingId);
-  } else if (window.location.pathname === "/bookings") {
-    showFavouriteBookings();
-  }
+    const regex = /^\/bookings\/([0-9]+)$/i;
+    const match = window.location.pathname.match(regex);
+    if (match && match.length === 2) {
+        const bookingId = Number(match[1]);
+        addBookingToFavourites(bookingId);
+    } else if (window.location.pathname === "/bookings") {
+        showFavouriteBookings();
+    }
 });
