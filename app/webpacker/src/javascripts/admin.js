15e3f3dc0ff59416573a3998f43f40082adb87dc
Charlie Jonas <charlie@charliejonas.co.uk>
2019-08-21 20:17:45 +0100

    Various fixes to CSS and JS assets

--- app/webpacker/src/javascripts/admin.js
+++ app/webpacker/src/javascripts/admin.js
@@ -5,13 +5,13 @@ $(() => {
     $("a#new-term").on("ajax:success", (event) => {
         rbModal("Start New Term", "Previous shows are now being marked as dormant in the background – please refresh the page after a few seconds...");
     });
-    $('[id$="max_rehearsals"]').on('change', () => {
+    $('[id$="max_rehearsals"]').on('change', function() {
         Rails.fire(this.form, "submit");
     });
-    $('[id$="max_auditions"]').on('change', () => {
+    $('[id$="max_auditions"]').on('change', function() {
         Rails.fire(this.form, "submit");
     });
-    $('[id$="max_meetings"]').on('change', () => {
+    $('[id$="max_meetings"]').on('change', function() {
         Rails.fire(this.form, "submit");
     });
 });
