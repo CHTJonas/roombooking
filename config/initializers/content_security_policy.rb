116354be28c93951ce71a865093b7f9d8b6a8f08
Charlie Jonas <charlie@charliejonas.co.uk>
2019-08-30 22:18:53 +0100

    Disable CSP for now
    Can reintroduce in the future perhaps. It was a nice experiment but it just floods Sentry with error reports.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,34 +1,34 @@
 # frozen_string_literal: true
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src     :none
-  policy.img_src         :self, "https://secure.gravatar.com"
-  policy.script_src      :self, "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/recaptcha/api.js"
-  policy.frame_src       "https://www.google.com/recaptcha/"
-  policy.style_src       :self, :unsafe_inline
-  policy.font_src        :self
-  policy.form_action     :self, "https://www.camdram.net"
-  policy.frame_ancestors :none
-  policy.base_uri        :none
-  policy.block_all_mixed_content
+# Rails.application.config.content_security_policy do |policy|
+#   policy.default_src     :none
+#   policy.img_src         :self, "https://secure.gravatar.com"
+#   policy.script_src      :self, "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/recaptcha/api.js"
+#   policy.frame_src       "https://www.google.com/recaptcha/"
+#   policy.style_src       :self, :unsafe_inline
+#   policy.font_src        :self
+#   policy.form_action     :self, "https://www.camdram.net"
+#   policy.frame_ancestors :none
+#   policy.base_uri        :none
+#   policy.block_all_mixed_content
 
-  if Rails.env.development?
-    policy.connect_src   :self, "http://localhost:3035", "ws://localhost:3035", "https://sentry.io"
-  else
-    policy.connect_src   :self, "https://sentry.io"
-  end
+#   if Rails.env.development?
+#     policy.connect_src   :self, "http://localhost:3035", "ws://localhost:3035", "https://sentry.io"
+#   else
+#     policy.connect_src   :self, "https://sentry.io"
+#   end
 
-  if Rails.env.production?
-    policy.upgrade_insecure_requests
-  end
+#   if Rails.env.production?
+#     policy.upgrade_insecure_requests
+#   end
 
-  if ENV['CSP_REPORT_URI'].present?
-    policy.report_uri    ENV['CSP_REPORT_URI']
-  end
-end
+#   if ENV['CSP_REPORT_URI'].present?
+#     policy.report_uri    ENV['CSP_REPORT_URI']
+#   end
+# end
 
-# Enable automatic crytographic nonce generation for UJS.
-Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# # Enable automatic crytographic nonce generation for UJS.
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
-# Don't enforce CSP but report violations instead.
-Rails.application.config.content_security_policy_report_only = true
+# # Don't enforce CSP but report violations instead.
+# Rails.application.config.content_security_policy_report_only = true
