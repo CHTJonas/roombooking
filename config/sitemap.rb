4eaac11acacadc2ed1315faeba4321de18a777da
Charlie Jonas <charlie@charliejonas.co.uk>
2019-12-03 23:22:48 +0000

    Allow indexing by search engines

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,6 +3,10 @@
 SitemapGenerator::Sitemap.default_host = Roombooking::Host.url
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.create do
+  add '/about', changefreq: 'weekly'
+  add '/faq', changefreq: 'weekly'
+  add '/privacy', changefreq: 'weekly'
+  add '/contact', changefreq: 'weekly'
   Room.find_each do |room|
     add room_path(room), lastmod: room.updated_at, changefreq: 'hourly'
   end
