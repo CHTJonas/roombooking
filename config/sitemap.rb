841c2b7a42efe823d2a03ea0dd518708077f1b70
Charlie Jonas <charlie@charliejonas.co.uk>
2019-12-03 23:20:13 +0000

    Fix bug in sitemap generator
    The `find_each` method that was used previously had the side effect fo
    reversing the sort order of the SQL query. As the memory impact of
    loading 250 bookings should not be severe, this commit removes the
    `find_each` method to ensure the shows are fed to the generator in the
    correct order.
    
    Signed-off-by: Charlie Jonas <charlie@charliejonas.co.uk>

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,7 +6,7 @@ SitemapGenerator::Sitemap.create do
   Room.find_each do |room|
     add room_path(room), lastmod: room.updated_at, changefreq: 'hourly'
   end
-  Booking.order('id DESC').limit(250).find_each do |booking|
-    add booking_path(booking), lastmod: booking.updated_at, changefreq: 'daily'
+  Booking.limit(250).order(created_at: :desc).each do |booking|
+    add p, lastmod: booking.updated_at, changefreq: 'daily'
   end
 end
