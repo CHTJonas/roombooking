17e9458ee52b60f31945bb1dda1bb281d47a6160
Charlie Jonas <charlie@charliejonas.co.uk>
2019-12-09 23:13:30 +0000

    Fix select bugs and improve performance in show expiry job
    The previous iteration of the ShowExpiryJob class fell over in niche
    situations where a show on Room Booking was deleted from Camdram or
    where a show had no performances listed on Camdram. This commit fixes
    those two bugs and also improves performance by memoizing the authorised
    venues IDs and the value of `DateTime.now'.
    
    Signed-off-by: Charlie Jonas <charlie@charliejonas.co.uk>

--- app/jobs/show_expiry_job.rb
+++ app/jobs/show_expiry_job.rb
@@ -8,13 +8,16 @@ class ShowExpiryJob
   sidekiq_throttle concurrency: { limit: 1 }
 
   def perform
+    now = DateTime.now
+    venue_ids = CamdramVenue.all.map(&:camdram_id)
     CamdramShow.where(dormant: false).find_each do |show|
-      venue_ids = CamdramVenue.all.map(&:camdram_id)
-      performances = show.camdram_object.performances.select do |p|
+      camdram_object = show.camdram_object
+      next if camdram_object.nil?
+      performances = camdram_object.performances.select do |p|
         p.venue.present? && venue_ids.include?(p.venue.id)
       end
       last_performance = performances.sort { |p1, p2| p1.end_at - p2.end_at }.last
-      if DateTime.now > last_performance.end_at
+      if last_performance.present? && last_performance.end_at < now
         show.update(dormant: true)
       end
     end
