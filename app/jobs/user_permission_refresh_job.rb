f244c80dcff5a3fe819362f6e11774a7d55f2bae
Charlie Jonas <charlie@charliejonas.co.uk>
2020-02-10 20:54:43 +0000

    Only refresh user permissions if we have a currently valid Camdram API access token for that user.
    This commit fixes #446.
    
    Signed-off-by: Charlie Jonas <charlie@charliejonas.co.uk>

--- app/jobs/user_permission_refresh_job.rb
+++ app/jobs/user_permission_refresh_job.rb
@@ -10,7 +10,7 @@ class UserPermissionRefreshJob
   def perform
     User.eager_load(:latest_camdram_token).find_each do |user|
       begin
-        if user.admin? || user.latest_camdram_token.present?
+        if user.admin? || (user.latest_camdram_token.present? && !user.latest_camdram_token.expired?)
           user.refresh_permissions!
         end
       rescue => e
