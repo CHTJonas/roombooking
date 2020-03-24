b1affda1f63d905ea79a327eada65b535dedce2a
Charlie Jonas <charlie@charliejonas.co.uk>
2020-03-24 23:11:09 +0000

    Add additional unit tests to room model

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -19,4 +19,34 @@ class RoomTest < ActiveSupport::TestCase
     # cache warmup jobs.
     CamdramEntityCacheWarmupJob.clear
   end
+
+  test "should return a room's ordinary booking at a given date" do
+    room = rooms(:one)
+    user = users(:charlie)
+    time = DateTime.now.beginning_of_week + 10.hours
+    booking = room.get_booking_at(time)
+    assert_equal 'other', booking.purpose
+    assert_equal user, booking.user
+    assert_equal 'ordinary_booking_0', booking.name
+  end
+
+  test "should return a room's daily repeat booking at a given date" do
+    room = rooms(:one)
+    user = users(:charlie)
+    time = DateTime.now.beginning_of_week - 2.weeks + 16.hours
+    booking = room.get_booking_at(time)
+    assert_equal 'other', booking.purpose
+    assert_equal user, booking.user
+    assert_equal 'daily_repeat_booking_2', booking.name
+  end
+
+  test "should return a room's weekly booking at a given date" do
+    room = rooms(:one)
+    user = users(:charlie)
+    time = DateTime.now.beginning_of_week - 1.weeks + 9.hours + 30.minutes
+    booking = room.get_booking_at(time)
+    assert_equal 'other', booking.purpose
+    assert_equal user, booking.user
+    assert_equal 'weekly_repeat_booking_1', booking.name
+  end
 end
