d63614c8edc5b494fc6b844a3ba23aa476e81da6
Charlie Jonas <charlie@charliejonas.co.uk>
2020-09-29 23:35:33 +0100

    Refactor version info on about page

--- lib/roombooking/version.rb
+++ lib/roombooking/version.rb
@@ -8,6 +8,10 @@ module Roombooking
       def git_description
         @git_description ||= (`git describe --tags 2>/dev/null || git rev-parse --short HEAD`.chomp + `[[ -z $(git status -s) ]] || echo ' dirty'`.chomp).freeze
       end
+
+      def git_description_without_prefix
+        git_description.delete_prefix("v").freeze
+      end
     end
   end
 end
