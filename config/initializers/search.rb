c8e0c9c439e4a336ad35a5f1ccd820199c891c3d
Charlie Jonas <charlie@charliejonas.co.uk>
2020-10-11 18:33:15 +0100

    Revert "Fix threading bug with pg_search library"
    This reverts commit e793a9a946585cb5f0dd1cd277a6eb8e2e08dbbd.

--- config/initializers/search.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# See https://github.com/Casecommons/pg_search/issues/446
-module PgSearch
-  mattr_accessor :unaccent_function
-  self.unaccent_function = 'unaccent'
-end
