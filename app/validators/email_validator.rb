e635fda2981fda6672aa128d0e18d826fe4209e0
Charlie Jonas <charlie@charliejonas.co.uk>
2021-07-12 22:40:45 +0100

    Fix deprecated ActiveModel::Errors message array syntax

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -4,6 +4,8 @@ class EmailValidator < ActiveModel::EachValidator
   @@email_regexp = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || 'is not a valid email address') unless value =~ @@email_regexp
+    unless value =~ @@email_regexp
+      record.errors[attribute].add(options[:message] || 'is not a valid email address')
+    end
   end
 end
