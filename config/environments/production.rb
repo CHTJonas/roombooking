6041c8f750ff002bba251dfe4bda8cc7fe49bc5f
Charlie Jonas <charlie@charliejonas.co.uk>
2021-07-13 22:22:52 +0100

    Refactor caching code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,15 +13,12 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
-
-  # Serve static assets as the NGINX frontend only handles TLS.
-  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
