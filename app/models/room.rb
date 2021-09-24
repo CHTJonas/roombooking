6af3216442953e8af5148fbf3a0dbefab08adfaa
Charlie Jonas <charlie@charliejonas.co.uk>
2021-09-24 12:42:19 +0100

    More accurate error message

--- app/models/room.rb
+++ app/models/room.rb
@@ -58,8 +58,8 @@ class Room < ApplicationRecord
   private
 
   def can_destroy?
-    unless bookings.count == 0 || ENV['LET_ME_DESTROY_ROOMS'] == "YES_I_WANT_TO_LOSE_HISTORICAL_DATA"
-      self.errors.add(:base, "Room can't be destroyed because the system is configured to prevent the loss of historic booking data.")
+    unless bookings.count == 0 || ENV['LET_ME_DESTROY_ROOMS'] == "YES_I_WANT_TO_LOSE_BOOKING_DATA"
+      self.errors.add(:base, "Room can't be destroyed because the system is configured to prevent the loss of booking data.")
       throw :abort
     end
   end
