565d09867ddd81cb98e3006cf052afd389043c59
Charlie Jonas <charlie@charliejonas.co.uk>
2021-09-30 22:02:05 +0100

    Remove deprecated "Start new term" button code
    The original functionality that used this code is long since gone.

--- app/services/admin/show_retrieval_service.rb
+++ app/services/admin/show_retrieval_service.rb
@@ -9,10 +9,6 @@ module Admin
       camdram_shows.each do |camdram_show|
         roombooking_show = CamdramShow.find_from_camdram(camdram_show)
         if roombooking_show.try(:dormant?)
-          # Show are only marked dormant at the start/end of a term, in which
-          # case they should be absent from the response from the Camdram API
-          # (since they're no longer upcoming). Hence this operation won't be
-          # called very often is so shouldn't be too computationally expensive.
           show_tuples.delete_at(i)
         else
           show_tuples[i] = [camdram_show, roombooking_show]
--- app/webpacker/src/javascripts/admin.js
+++ app/webpacker/src/javascripts/admin.js
@@ -2,9 +2,6 @@ $(() => {
     $("a#batch-import").on("ajax:success", (event) => {
         rbModal("Batch Import", "The batch import job is now running in the background – please refresh the page after a few seconds...");
     });
-    $("a#new-term").on("ajax:success", (event) => {
-        rbModal("Start New Term", "Previous shows are now being marked as dormant in the background – please refresh the page after a few seconds...");
-    });
     $('[id$="max_rehearsals"]').on('change', function() {
         Rails.fire(this.form, "submit");
     });
