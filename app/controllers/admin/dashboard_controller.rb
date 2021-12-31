eefe056b03e6c6dfdfac374f7801a3a18918246d
Charlie Jonas <charlie@charliejonas.co.uk>
2021-12-31 01:15:10 +0000

    Add restart and shutdown handlers in prod

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,15 +8,29 @@ module Admin
       render html: Rails::Info.to_html.html_safe
     end
 
-    def restart; end
+    def restart
+      log_abuse "#{current_user.to_log_s} initiated a system restart"
+      if Rails.env.production?
+        `/usr/bin/sudo /bin/systemctl restart roombooking`
+      else
+        render plain: 'No restart handler available; refusing to do so.'
+      end
+    end
 
-    def shutdown; end
+    def shutdown
+      log_abuse "#{current_user.to_log_s} initiated a system shutdown"
+      if Rails.env.production?
+        `/usr/bin/sudo /bin/systemctl restart roombooking`
+      else
+        render plain: 'No shutdown handler available; refusing to do so.'
+      end
+    end
 
     def backup
       log_abuse "#{current_user.to_log_s} downloaded a database dump"
       begin
         send_data `pg_dump -Fc roombooking_#{Rails.env}`,
-                  filename: "roombooking_#{Rails.env}_#{Time.zone.now.to_i}.pgdump"
+          filename: "roombooking_#{Rails.env}_#{Time.zone.now.to_i}.pgdump"
       rescue StandardError => e
         Sentry.capture_exception(e)
         render plain: 'An error occurred when creating the backup!'
