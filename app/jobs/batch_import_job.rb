1a1094cf902afc0f9c8d1180f683ba3adb1d93cf
Charlie Jonas <charlie@charliejonas.co.uk>
2022-02-27 18:09:46 +0000

    Improve handling of block bookings during batch imports

--- app/jobs/batch_import_job.rb
+++ app/jobs/batch_import_job.rb
@@ -11,30 +11,29 @@ class BatchImportJob
     result = BatchImportResult.lock.find(result_id)
     result.update!(started: Time.now)
 
+    user = User.find(user_id)
+    Sentry.set_user({ id: user.id, name: user.name, email: user.email })
     PaperTrail.request.whodunnit = user_id
+
     shows = ShowEnumerationService.perform
     shows_imported_successfully = []
     shows_imported_unsuccessfully = []
     shows_already_imported = []
 
     shows.each do |camdram_show|
-      # First check if the show has already been imported.
+      # First check if the show has already been imported
+      # and if it has, we skip to the next one.
       if CamdramShow.find_by(camdram_id: camdram_show.id)
         shows_already_imported << camdram_show.id
         next
       end
 
-      # Now actually import the show, wrapped in a single
-      # transaction so that we either import the show and
-      # make its block bookings successfully, or we
-      # rollback and ignore that single show.
-      ActiveRecord::Base.transaction do
-        show = CamdramShow.create_from_camdram(camdram_show)
-        show.block_out_bookings(User.find(user_id))
-      end
+      # Show needs importing, so actually import it.
+      CamdramShow.create_from_camdram(camdram_show).block_out_bookings(user)
       shows_imported_successfully << camdram_show.id
-    rescue ActiveRecord::RecordInvalid => e
+    rescue => e
       shows_imported_unsuccessfully << camdram_show.id
+      Sentry.set_extras({ camdram_id: camdram_show.id })
       Sentry.capture_exception(e)
       next
     end
