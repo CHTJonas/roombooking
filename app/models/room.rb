156d8f20e11a59347385026be36d7cda366721c3
Charlie Jonas <charlie@charliejonas.co.uk>
2022-09-26 20:55:52 +0100

    Further DST bugfixes when finding a room's bookings

--- app/models/room.rb
+++ app/models/room.rb
@@ -31,22 +31,22 @@ class Room < ApplicationRecord
     get_booking_at(now)
   end
 
-  def get_booking_at(date)
-    query = bookings.find_by(repeat_mode: :none, start_time: Time.at(0)..date, end_time: date..DateTime::Infinity.new)
+  def get_booking_at(moment)
+    moment = Time.zone.local_to_utc(moment.in_time_zone('Europe/London'))
+    query = bookings.find_by(repeat_mode: :none, start_time: Time.at(0)..moment, end_time: moment..DateTime::Infinity.new)
     return query unless query.nil?
 
-    secs = date.in_time_zone('Europe/London').seconds_since_midnight
-    bookings.where(repeat_mode: :daily, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
-      next unless bkg.start_time.seconds_since_midnight < secs
-      if bkg.end_time.seconds_since_midnight > secs || bkg.end_time.seconds_since_midnight == 0
+    bookings.where(repeat_mode: :daily, start_time: Time.at(0)..moment, repeat_until: moment..DateTime::Infinity.new).each do |bkg|
+      next unless bkg.start_time.seconds_since_midnight < moment.seconds_since_midnight
+      if bkg.end_time.seconds_since_midnight > moment.seconds_since_midnight || bkg.end_time.seconds_since_midnight == 0
         return bkg
       end
     end
 
-    bookings.where(repeat_mode: :weekly, start_time: Time.at(0)..date, repeat_until: date..DateTime::Infinity.new).each do |bkg|
-      next unless bkg.start_time.wday == date.wday
-      next unless bkg.start_time.seconds_since_midnight < secs
-      if bkg.end_time.seconds_since_midnight > secs || bkg.end_time.seconds_since_midnight == 0
+    bookings.where(repeat_mode: :weekly, start_time: Time.at(0)..moment, repeat_until: moment..DateTime::Infinity.new).each do |bkg|
+      next unless bkg.start_time.wday == moment.wday
+      next unless bkg.start_time.seconds_since_midnight < moment.seconds_since_midnight
+      if bkg.end_time.seconds_since_midnight > moment.seconds_since_midnight || bkg.end_time.seconds_since_midnight == 0
         return bkg
       end
     end
